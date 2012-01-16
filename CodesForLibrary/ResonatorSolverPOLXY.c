1eab12c2631121e4ce67ee5f27daeff5048fb222
xdliang <xdliang@mit.edu>
2012-01-16 18:26:48 -0500

    create MLDOSPOLXY files
    Ignore-this: b99f9217954ad0e503930aed71871f1c
    
    Create MLDOSPOLXY files to compute the ldos at single source with two polarizations. To be implemented.
    
    darcs-hash:20120116232648-88e91-b67184349e99d5754095eca8978812701f7d6e15.gz

---
+++ CodesForLibrary/ResonatorSolverPOLXY.c
@@ -0,0 +1,211 @@
+#include <petsc.h>
+#include <time.h>
+#include "Resonator.h"
+
+extern int count;
+extern int its;
+extern int maxit;
+extern int ccount;
+extern double cldos;
+
+#undef __FUNCT__ 
+#define __FUNCT__ "ResonatorSolver"
+double ResonatorSolver(int Mxyz,double *epsopt, double *grad, void *data)
+{
+  
+  PetscErrorCode ierr;
+  
+  myfundatatype *ptmyfundata = (myfundatatype *) data;
+  
+  int Nx = ptmyfundata->SNx;
+  int Ny = ptmyfundata->SNy;
+  int Nz = ptmyfundata->SNz;
+  double hx = ptmyfundata->Shx;
+  double hy = ptmyfundata->Shy;
+  double hz = ptmyfundata->Shz;
+  double omega = ptmyfundata->Somega;
+  KSP ksp = ptmyfundata->Sksp;
+  Vec epspmlQ = ptmyfundata->SepspmlQ;
+  Vec epsmedium = ptmyfundata->Sepsmedium;
+  Vec epsC = ptmyfundata->SepsC;
+  Vec epsCi = ptmyfundata->SepsCi;
+  Vec epsP = ptmyfundata->SepsP;
+  Vec x = ptmyfundata->Sx;
+  Vec b = ptmyfundata->Sb;
+  Vec weightedJ = ptmyfundata->SweightedJ;
+  Vec vR = ptmyfundata->SvR;
+  Vec epsSReal = ptmyfundata->SepsSReal;
+
+  Mat A = ptmyfundata->SA;
+  Mat D = ptmyfundata->SD;
+  Mat M = ptmyfundata->SM;
+  
+  char *filenameComm = ptmyfundata->SfilenameComm;
+
+  int Nxyz = Nx*Ny*Nz;
+  double hxyz = (Nz==1)*hx*hy + (Nz>1)*hx*hy*hz;
+
+  // copy epsopt to epsSReal;
+  ierr=ArrayToVec(epsopt, epsSReal); CHKERRQ(ierr);
+ 
+
+  // Update the diagonals of M Matrix;
+  ModifyMatDiagonals(M, A,D, epsSReal, epspmlQ, epsmedium, epsC, epsCi, epsP, Nxyz,omega);
+   
+
+  
+  #if 1
+  //clock_t tstart, tend;  int tpast; tstart=clock();  
+  PetscLogDouble t1,t2,tpast;
+  ierr = PetscGetTime(&t1);CHKERRQ(ierr);
+  #endif
+  /*-----------------KSP Solving------------------*/ 
+
+#if 1 
+  if (its> 15 || count< 15 )
+    {
+      PetscPrintf(PETSC_COMM_WORLD,"Same nonzero pattern, LU is redone! \n");
+      ierr = KSPSetOperators(ksp,M,M,SAME_NONZERO_PATTERN);CHKERRQ(ierr);}
+  else
+    {ierr = KSPSetOperators(ksp,M,M,SAME_PRECONDITIONER);CHKERRQ(ierr);}
+
+   ierr = KSPSolve(ksp,b,x);CHKERRQ(ierr);
+   ierr = KSPGetIterationNumber(ksp,&its);CHKERRQ(ierr);
+   ierr = PetscPrintf(PETSC_COMM_WORLD,"--- the number of Kryolv Iterations in this step is %D----\n ",its);CHKERRQ(ierr);
+
+#endif
+
+   // if GMRES is stopped due to maxit, then redo it with sparse direct solve;
+#if 1
+  {
+    ierr = KSPGetIterationNumber(ksp,&its);CHKERRQ(ierr);
+    if(its>(maxit-2))
+      {
+	PetscPrintf(PETSC_COMM_WORLD,"Too many iterations needed! Recomputing \n");
+	ierr = KSPSetOperators(ksp,M,M,SAME_NONZERO_PATTERN);CHKERRQ(ierr);
+	ierr = KSPSolve(ksp,b,x);CHKERRQ(ierr);
+	ierr = KSPGetIterationNumber(ksp,&its);CHKERRQ(ierr);
+	ierr = PetscPrintf(PETSC_COMM_WORLD,"--- the number of Kryolv Iterations in this step is %D---\n ",its);CHKERRQ(ierr);
+     }
+  }
+#endif
+
+
+  //Print kspsolving information
+  //int its;
+
+#if 1
+  double norm;
+  Vec xdiff;
+  ierr=VecDuplicate(x,&xdiff);CHKERRQ(ierr);
+  ierr = MatMult(M,x, xdiff);CHKERRQ(ierr);
+  ierr = VecAXPY(xdiff,-1.0,b);CHKERRQ(ierr);
+  ierr = VecNorm(xdiff,NORM_INFINITY,&norm);CHKERRQ(ierr);
+  //ierr = KSPGetIterationNumber(ksp,&its);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"---Norm of error %A, Kryolv Iterations %D----\n ",norm,its);CHKERRQ(ierr);    
+  ierr=VecDestroy(xdiff);CHKERRQ(ierr);
+#endif
+
+  /*--------------Finish KSP Solving---------------*/
+#if 1
+  ierr = PetscGetTime(&t2);CHKERRQ(ierr);
+  tpast = t2 - t1;
+
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  if(rank==0)
+  PetscPrintf(PETSC_COMM_SELF,"---The runing time is %f s \n",tpast);
+#endif   
+
+
+  double ldos; //ldos = -Re((weight.*J)'*E) or -Re(E'*(weight*J));
+  ierr = VecDot(x,weightedJ,&ldos);
+  ldos = -1.0*ldos*hxyz;
+  PetscPrintf(PETSC_COMM_WORLD,"---The current ldos at step %d is %.16e \n", count,ldos);
+  PetscPrintf(PETSC_COMM_WORLD,"-------------------------------------------------------------- \n");
+
+ 
+  /* Now store the epsilon at each step*/
+  char buffer [100];
+
+  int STORE=1;    
+ if(STORE==1)
+    {
+      sprintf(buffer,"%.5depsSReal.m",count);
+      OutputVec(PETSC_COMM_WORLD, epsSReal, filenameComm, buffer);      
+    }
+
+ /* Now store the epsilon which makes improvement */
+ int cSTORE=0;
+ if (cSTORE==1 && ldos > cldos)
+   {
+     sprintf(buffer,"%.5depsSRealMONT.m",ccount);
+     OutputVec(PETSC_COMM_WORLD, epsSReal, filenameComm, buffer);
+     cldos=ldos;
+     ccount++;
+   }
+ 
+
+
+
+  /*-----take care of the gradient-------*/
+  if (grad) {
+   Vec epscoef = ptmyfundata->Sepscoef;
+   Vec epsgrad = ptmyfundata->Sepsgrad;
+   Vec vgrad = ptmyfundata->Svgrad;
+   Vec vgradlocal = ptmyfundata->Svgradlocal;
+   Vec tmp = ptmyfundata->Stmp;
+   Vec tmpa = ptmyfundata->Stmpa;
+   Vec tmpb = ptmyfundata->Stmpb;
+   VecScatter scatter = ptmyfundata->Sscatter;
+   IS from = ptmyfundata->Sfrom;
+   IS to = ptmyfundata->Sto;
+
+#if 1
+   /* Adjoint-Method tells us Mtran*lambba =J -> x = i*omega/weight*conj(lambda);  therefore the derivative is Re(x^2*weight*i*omega*(1+i/Qabs)*epspml) = Re(x^2*epscoef) ; here, I omit two minus signs: one is M'*lam= -j; the other is -Re(***). minus minus is a plus.*/
+   int aconj=0;
+   CmpVecProd(x,epscoef,tmp,D,Nxyz,aconj,tmpa,tmpb);
+   CmpVecProd(x,tmp,epsgrad,D,Nxyz,aconj,tmpa,tmpb);   
+   VecScale(epsgrad,hxyz); // the factor hxyz handle both 2D and 3D;
+#endif
+
+
+#if 0
+ /* Adjoint-Method tells us Mtran*lambda = -weight.*J= -weightedJ; therefore we have to solve M*conj(lambda) = -weightedJ; then the derivative is -conj(lambda).*[-omega^2*epspml*(1+i/Qabs)]*x); it is equivalent to solve M*z = weightedJ, and derivative is z*epspml*(1+i/Qabs)*omega^2*x = z*epspmlQ*x*omega^2; */
+    Vec cglambda = ptmyfundata->Scglambda;
+   int its2;
+   ierr = KSPSolve(ksp,weightedJ,cglambda);CHKERRQ(ierr);
+   ierr = KSPGetIterationNumber(ksp,&its2);CHKERRQ(ierr);
+   ierr = PetscPrintf(PETSC_COMM_WORLD,"--- the number of Kryolv Iterations for Adjoint equation is %D----\n ",its2);CHKERRQ(ierr);
+   int aconj=0;
+   CmpVecProd(cglambda,epspmlQ,tmp,D,Nxyz,aconj,tmpa,tmpb);
+   CmpVecProd(x,tmp,epsgrad,D,Nxyz,aconj,tmpa,tmpb); 
+   VecScale(epsgrad,-pow(omega,2)*hx*hy); // the minus sign is because the quation we are solving is M z = -weightedJ; // the factor hx*hy is from 2D intergartion;
+#endif
+
+
+   // set imaginary part of epsgrad = 0; ( we're only interested in real part;
+   ierr = VecPointwiseMult(epsgrad,epsgrad,vR); CHKERRQ(ierr);
+
+   // vgrad =A'*epsgrad; A' is the restriction matrix; Mapped to the small grid;
+   ierr = MatMultTranspose(A,epsgrad,vgrad);CHKERRQ(ierr);   
+
+// copy vgrad (distributed vector) to a regular array grad;
+   ierr = VecToArray(vgrad,grad,scatter,from,to,vgradlocal,Mxyz);
+  }
+
+  count++;
+
+  return ldos;
+}
+
+
+
+
+
+
+
+
+
+
+
---
+++ MLDOSPOLXY/Resonator.h
@@ -0,0 +1,146 @@
+//define Global variables;
+
+// from MoperatorGeneral.c
+PetscErrorCode MoperatorGeneral(MPI_Comm comm, Mat *Mout, int Nx, int Ny, int Nz, double hx, double hy, double hz, int bx[2], int by[2], int bz[2], double *muinv,int DimPeriod);
+
+// from SourceGeneration.c
+PetscErrorCode SourceSingleSetX(MPI_Comm comm, Vec J, int Nx, int Ny, int Nz, int scx, int scy, int scz, double amp);
+
+PetscErrorCode SourceSingleSetY(MPI_Comm comm, Vec J, int Nx, int Ny, int Nz, int scx, int scy, int scz, double amp);
+
+PetscErrorCode SourceSingleSetZ(MPI_Comm comm, Vec J, int Nx, int Ny, int Nz, int scx, int scy, int scz, double amp);
+
+
+PetscErrorCode SourceDuplicate(MPI_Comm comm, Vec *bout, int Nx, int Ny, int Nz, int scx, int scy, int scz, double amp);
+
+PetscErrorCode SourceBlock(MPI_Comm comm, Vec *bout, int Nx, int Ny, int Nz, double hx, double hy, double hz, double lx, double ux, double ly, double uy, double lz, double uz, double amp);
+
+// from PML.c
+double pmlsigma(double RRT, double d);
+
+PetscErrorCode EpsPMLFull(MPI_Comm comm, Vec epspml, int Nx, int Ny, int Nz, int Npmlx, int Npmly, int Npmlz, double sigmax, double sigmay, double sigmaz, double omega, int LowerPML);
+
+PetscErrorCode MuinvPMLFull(MPI_Comm comm, Vec *muinvout, int Nx, int Ny, int Nz, int Npmlx, int Npmly, int Npmlz, double sigmax, double sigmay, double sigmaz, double omega, int LowerPML);
+
+// from Eps.c 
+PetscErrorCode EpsCombine(Mat D, Vec weight, Vec epspml, Vec epspmlQ, Vec epsgrad, double Qabs,double omega);
+
+PetscErrorCode ModifyMatDiagonals(Mat M, Mat A, Mat D, Vec epsSReal, Vec epspmlQ, Vec epsmedium, Vec epsC, Vec epsCi, Vec epsP, int Nxyz, double omega);
+
+PetscErrorCode ModifyMatDiagonalsForOmega( Mat M, Mat A, Mat D, Vec epsSReal, Vec epspmlQ, Vec epsC, Vec epsCi, Vec epsP, int Nxyz, double omegasqr);
+
+//PetscErrorCode  yee_interp(MPI_Comm comm, Mat *Aout, int Nx, int Ny, int Nz, double x0, double y0, double z0, double x1, double y1, double z1, int Mx, int My, int Mz);
+
+PetscErrorCode  myinterp(MPI_Comm comm, Mat *Aout, int Nx, int Ny, int Nz, int Nxo, int Nyo, int Nzo, int Mx, int My, int Mz, int Mzslab);
+
+//PetscErrorCode myinterpSlab(MPI_Comm comm, Mat *Aout, int Nx, int Ny, int Nz, int Nxo, int Nyo, int Nzo, int Mx, int My, int Mz, int Mzslab);
+
+//PetscErrorCode General_interp(MPI_Comm comm, Mat *Aout, int Nx, int Ny, int Nz, double hx, double hy, double hz, double x0, double y0, double z0, double x1, double y1, double z1, int Mx, int My, int Mz);
+
+
+// from MathTools.c
+PetscErrorCode  CmpVecProd(Vec va, Vec vb, Vec vout, Mat D, int Nxyz, int aconj, Vec vai, Vec vbi);
+
+PetscErrorCode  ImagIMat(MPI_Comm comm, Mat *Dout, int Nxyz);
+
+PetscErrorCode   CongMat(MPI_Comm comm, Mat *Cout, int Nxyz);
+
+PetscErrorCode   GetWeightVec(Vec weight,int Nx, int Ny, int Nz);
+
+PetscErrorCode GetMediumVec(Vec epsmedium, int Nz, int Mz, double epsair, double epssub);
+
+PetscErrorCode  GetRealPartVec(Vec vR, int Nxyz);
+
+PetscErrorCode  ArrayToVec(double *pt, Vec V);
+
+PetscErrorCode VecToArray(Vec V, double *pt, VecScatter scatter, IS from, IS to, Vec Vlocal, int Mxyz);
+
+PetscErrorCode AddMuAbsorption(double *muinv, Vec muinvpml, double Qabs, int Nxyz, int add);
+
+// from Output.c
+PetscErrorCode  OutputVec(MPI_Comm comm, Vec x, const char *filenameComm, const char *filenameProperty);
+
+PetscErrorCode  OutputMat(MPI_Comm comm, Mat A, const char *filenameComm, const char *filenameProperty);
+
+PetscErrorCode RetrieveVecPoints(Vec x, int Npt, int *Pos, double *ptValues);
+
+PetscErrorCode MyCheckAndOutputInt(PetscTruth flg, int CmdVar, const char *strCmdVar, const char *strCmdVarDetail);
+
+PetscErrorCode MyCheckAndOutputDouble(PetscTruth flg, double CmdVar, const char *strCmdVar, const char *strCmdVarDetail);
+
+PetscErrorCode MyCheckAndOutputChar(PetscTruth flg, char *CmdVar, const char *strCmdVar, const char *strCmdVarDetail);
+
+
+
+PetscErrorCode GetIntParaCmdLine(int *ptCmdVar, const char *strCmdVar, const char *strCmdVarDetail);
+
+
+// from ResonatorSolver.c
+double ResonatorSolver(int Mxyz, double *epsopt, double *grad, void *data);
+
+// from ldos.c
+double ldos(int numofvar, double *epsopt, double *grad, void *data);
+double ldosdiff(int numofvar, double *epsopt, double *grad, void *data);
+
+// from EigenSolver.c
+PetscErrorCode EigenSolver(void *data, int Linear, int Eig, int maxeigit);
+PetscErrorCode RayleighQuotient(Mat M, Vec diagB, Vec x, Vec b, Vec vR, Mat D, Vec tmpa, Vec tmpb, int N, int i);
+
+// from ResonatorSolverRHS.c
+double ResonatorSolverRHS(int Mxyz,double *epsopt, double *grad, void *data);
+
+
+// datatype used for optimization of dielectric structure;
+typedef struct { 
+  int SNx, SNy, SNz; 
+  double Shx, Shy, Shz;
+  double Somega;
+  KSP Sksp;
+  Vec SepspmlQ, Sepscoef, Sepsmedium, SepsC, SepsCi, SepsP, Sx, Scglambda, Sb, SweightedJ, SvR, SepsSReal, Sepsgrad, Svgrad, Svgradlocal, Stmp, Stmpa, Stmpb;
+  Mat SA, SD, SM;
+  IS Sfrom, Sto;
+  VecScatter Sscatter;
+  char *SfilenameComm;
+} myfundatatype;
+
+// dataype used for optimization of frequency;
+typedef struct { 
+  int SNx, SNy, SNz; 
+  double Shx, Shy, Shz;
+  double Somega, Ssparedouble;
+  double *Sspareptdouble;
+  KSP Sksp;
+  Vec SepspmlQ, Sepscoef, SepsC, SepsCi, SepsP, Sx, Scglambda, Sb, SweightedJ, SvR, SepsSReal, Sepsgrad, Svgrad, Svgradlocal, Stmp, Stmpa, Stmpb;
+  Mat SA, SD, SM;
+  IS Sfrom, Sto;
+  VecScatter Sscatter;
+  char *SfilenameComm;
+} myfundatatypeq;
+
+// datatype used for optimization of dielectric structure with mullitple RHS;
+typedef struct { 
+  int SNx, SNy, SNz, Sclx,Scly,Sclz, Sncx, Sncy, Sncz, SJdirection; 
+  double Shx, Shy, Shz;
+  double Somega;
+  KSP Sksp;
+  Vec SepspmlQ, Sepscoef, Sepsmedium, SepsC, SepsCi, SepsP, Sx, Scglambda, SJ, Sb, SweightedJ, SvR, SepsSReal, Sepsgrad, Svgrad, Svgradlocal, Stmp, Stmpa, Stmpb;
+  Mat SA, SD, SM;
+  IS Sfrom, Sto;
+  VecScatter Sscatter;
+  char *SfilenameComm;
+} myfundataRHStype;
+
+// dataype used for optimization of frequency with multiple RHS;
+typedef struct { 
+  int SNx, SNy, SNz,Sclx, Scly, Sclz, Sncx,Sncy,Sncz, SJdirection; 
+  double Shx, Shy, Shz;
+  double Somega, Ssparedouble;
+  double *Sspareptdouble;
+  KSP Sksp;
+  Vec SepspmlQ, Sepscoef, SepsC, SepsCi, SepsP, Sx, Scglambda, SJ, Sb, SweightedJ, SvR, SepsSReal, Sepsgrad, Svgrad, Svgradlocal, Stmp, Stmpa, Stmpb;
+  Mat SA, SD, SM;
+  IS Sfrom, Sto;
+  VecScatter Sscatter;
+  char *SfilenameComm;
+} myfundataRHStypeq;
+
