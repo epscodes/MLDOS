47862c137d9b486bddda6334f63cf87ec9eb827f
xdliang <xdliang@mit.edu>
2012-02-24 14:27:55 -0500

    implement MoperatorGeneralBloch to handle bloch period bc (first draft)
    Ignore-this: 82c7cf940bd702ea759d89c736884239
    
    darcs-hash:20120224192755-88e91-0cdac867d5f24a1e507d3650a509d5b4641c44b3.gz

--- CodesForLibrary/MoperatorGeneralBloch.c
+++ CodesForLibrary/MoperatorGeneralBloch.c
@@ -1,6 +1,10 @@
 #include <petsc.h>
+#include <complex.h>
+
+typedef double complex dcomp;
 
 /*
+Generalize the MoperatorGeneral to handle the Bloch Boundary Condition;
 
 This rountine generates sparse matrix for the operator Curl \times 1/mu \times Curl (finite difference with Yee grid for a cubic domain):
 
@@ -21,7 +25,7 @@ Output parameter: sparse matrix M.
 
 #undef __FUNCT__ 
 #define __FUNCT__ "MoperatorGeneral"
-PetscErrorCode MoperatorGeneral(MPI_Comm comm, Mat *Aout, int Nx, int Ny, int Nz, double hx, double hy, double hz, int bx[2], int by[2], int bz[2], double *muinv, int DimPeriod)
+PetscErrorCode MoperatorGeneralBloch(MPI_Comm comm, Mat *Aout, int Nx, int Ny, int Nz, double hx, double hy, double hz, int bx[2], int by[2], int bz[2], double *muinv, int DimPeriod, double blochbc[3][2])
  /* bx[lo/hi = 0/1] = 0/1/-1: dirichlet/even/odd */
 {
   Mat A;
@@ -34,9 +38,10 @@ PetscErrorCode MoperatorGeneral(MPI_Comm comm, Mat *Aout, int Nx, int Ny, int Nz
   int Nxyzcr = Nx*Ny*Nz*Nc*Nr;
   int b[3][2][3]; /* b[x/y/z direction][lo/hi][Ex/Ey/Ez] */
   int Nxyz[3]={Nx,Ny,Nz};
-  double muinva, muinvl;
 
-
+  dcomp muinvcp1cmp, muinvcp1lcmp, muinvcp2cmp, muinvcp2lcmp;
+  dcomp cidu_phase, cp1idu_phase, cp1idl_phase, cp2idu_phase, cp2idl_phase;
+  double c1, c2, c3, c4;
    /*-------------------------------------*/
 
 
@@ -58,14 +63,12 @@ PetscErrorCode MoperatorGeneral(MPI_Comm comm, Mat *Aout, int Nx, int Ny, int Nz
       for(k=0; k<3; k++){
 	b[ic][j][k] =  ( (ic==0)*bx[j] + (ic==1)*by[j] + (ic==2)*bz[j])*( k==ic ? -1 :1);
       }
-  
+
   for (i = ns; i < ne; ++i) {
     int ixyz[3], ic, ir, jr;
     int itmp;
     int cp1, cp2, icp1, icp2, cidu, cp1idu,cp1idl, cp2idu, cp2idl;
-    double cid_usign, cp1id_usign, cp1id_lsign, cp2id_usign, cp2id_lsign;
-
-	  
+    	  
     ixyz[2] = (itmp = i) % Nz;
     ixyz[1] = (itmp /= Nz) % Ny;
     ixyz[0] = (itmp /= Ny) % Nx;
@@ -91,70 +94,78 @@ PetscErrorCode MoperatorGeneral(MPI_Comm comm, Mat *Aout, int Nx, int Ny, int Nz
     cp2id = (ic==1)*Ny*Nz + (ic==2)*Nz + (ic==0);
    
 
-    cid_usign = 1.0;
-    cp1id_usign = 1.0;
-    cp1id_lsign = 1.0;
-    cp2id_usign = 1.0;
-    cp2id_lsign = 1.0;
+    cidu_phase = 1.0;
+    cp1idu_phase = 1.0;
+    cp1idl_phase = 1.0;
+    cp2idu_phase = 1.0;
+    cp2idl_phase = 1.0;
     
-
     for(jr=0; jr<2; jr++) { /* column real/imag parts */
-       int sign = (jr == 0) ? 1 : (ir == 0 ? -1 : 1); 
       int jrd =  (jr-ir)*Nxyzc;
-      int jrdmu =  ((jr == 0) ? 0 : (ir == 0 ? 1 : -1))*Nxyzc;
-     
-             
+               
+      dcomp magicnum = (ir==jr)*1 + (ir<jr)*I + (ir>jr)*(-I); 
 
       /* d/dy muinv d/dx Ey */
 
       if(ixyz[ic] == Nxyz[ic]-1)
 	{
 	  if(ic == (DimPeriod-1) || DimPeriod == 4 || (DimPeriod<0 && ic !=-(DimPeriod+1)) )
-	    cidu = (1-Nxyz[ic])*cid; // periodic b.c; DimPeriod ==4 means period in all three dimensions;
+	    {
+	      cidu = (1-Nxyz[ic])*cid; // periodic b.c; DimPeriod ==4 means period in all three dimensions;
+	      cidu_phase = blochbc[ic][0] + I*blochbc[ic][1];
+	    }
 	  else
 	    {
 	      cidu = 0;
-	      cid_usign = b[ic][1][cp1];
+	      cidu_phase = b[ic][1][cp1];
 	    }
 	}
- 
-      muinva = muinv[icp2 + jrdmu]; 
+
+	 muinvcp2cmp = muinv[icp2%Nxyzc] + I*muinv[icp2%Nxyzc + Nxyzc];
 
       if(ixyz[cp1]==0)
 	{
 	  if(cp1 == (DimPeriod-1) || DimPeriod == 4 || (DimPeriod<0 && cp1!=-(DimPeriod+1)) )
 	    {
 	      cp1idl = (1-Nxyz[cp1])*cp1id;
+	      cp1idl_phase = blochbc[cp1][0] - I*blochbc[cp1][1];
 	    }
 	  else
 	    {
 	      cp1idl = 0;
-	      cp1id_lsign = b[cp1][0][cp1];
+	      cp1idl_phase = b[cp1][0][cp1];
 	    }
 	}
 
-      muinvl = muinv[icp2 - cp1idl + jrdmu];
+      muinvcp2lcmp = muinv[(icp2 - cp1idl)%Nxyzc] + I*muinv[(icp2 - cp1idl)%Nxyzc + Nxyzc];
 
-      hh = sign * h[ic]*h[cp1];
+      hh = h[ic]*h[cp1];
+      c1 = +creal(cidu_phase * muinvcp2cmp * magicnum)/hh;
+      c2 = -creal(muinvcp2cmp * magicnum)/hh;
+      c3 = -creal(cidu_phase * cp1idl_phase * muinvcp2lcmp * magicnum)/hh;
+      c4 = +creal(cp1idl_phase * muinvcp2lcmp * magicnum)/hh;
+      
+      ierr = MatSetValue(A,i, icp1 + cidu + jrd,  c1, ADD_VALUES); CHKERRQ(ierr);
+      ierr = MatSetValue(A,i, icp1 + jrd, c2, ADD_VALUES); CHKERRQ(ierr);
+      ierr = MatSetValue(A,i, icp1 + cidu - cp1idl + jrd, c3, ADD_VALUES); CHKERRQ(ierr);
+      ierr = MatSetValue(A,i, icp1 - cp1idl + jrd, c4, ADD_VALUES); CHKERRQ(ierr);
 
-      ierr = MatSetValue(A,i, icp1 + cidu + jrd,  cid_usign*muinva/hh, ADD_VALUES); CHKERRQ(ierr);     
-       ierr = MatSetValue(A,i, icp1 + jrd, -muinva/hh, ADD_VALUES); CHKERRQ(ierr);
-       ierr = MatSetValue(A,i, icp1 + cidu - cp1idl + jrd, -cid_usign*cp1id_lsign*muinvl/ hh, ADD_VALUES); CHKERRQ(ierr);
-      ierr = MatSetValue(A,i, icp1 - cp1idl + jrd, +cp1id_lsign*muinvl/ hh, ADD_VALUES); CHKERRQ(ierr);
-	    
-    
+
+  
       /* - d/dy muinv d/dy Ex */
-
-      muinva = muinv[icp2 + jrdmu];
+      // it shares same muinvcp2cmp and muinvcp2lcmp as d/dx muinv d/dx Ey;
 
       if(ixyz[cp1] == Nxyz[cp1]-1)
 	{
 	  if( cp1== (DimPeriod-1) || DimPeriod == 4 || (DimPeriod<0 && cp1!=-(DimPeriod+1)) )
-	    cp1idu = (1-Nxyz[cp1])*cp1id;
+	    {
+	      cp1idu = (1-Nxyz[cp1])*cp1id;
+	      cp1idu_phase = blochbc[cp1][0] + I*blochbc[cp1][1];
+	    }
 	  else
 	    {
 	      cp1idu = 0;
-	      cp1id_usign = b[cp1][1][ic];
+	      cp1idu_phase = b[cp1][1][ic];
 	    }
 	}
 	       
@@ -163,25 +174,24 @@ PetscErrorCode MoperatorGeneral(MPI_Comm comm, Mat *Aout, int Nx, int Ny, int Nz
 	  if(cp1!= (DimPeriod-1) && DimPeriod !=4 && ( DimPeriod >=0 || cp1 == -(DimPeriod+1)) )
 	    {
 	      cp1idl = -cp1idu; 
-	      cp1id_lsign = b[cp1][0][ic];
-	      muinvl = muinva;
+	      cp1idl_phase = b[cp1][0][ic];
 	    }
 	  else
 	    {
 	      cp1idl = (1-Nxyz[cp1])*cp1id;
-	      muinvl = muinv[icp2-cp1idl+jrdmu];
+	      cp1idl_phase = blochbc[cp1][0] - I*blochbc[cp1][1];
 	    }
 	 }
-      else
-	{
-	  muinvl = muinv[icp2-cp1idl+jrdmu];
-	}
+     
+      hh =  h[cp1]*h[cp1];
+      c1 = -creal(cp1idu_phase * muinvcp2cmp * magicnum)/hh;
+      c2 = +creal( (muinvcp2cmp + muinvcp2lcmp) * magicnum)/hh;
+      c3 = -creal(cp1idl_phase * muinvcp2lcmp * magicnum)/hh;
+     
 
-
-      hh = sign * h[cp1]*h[cp1];
-      ierr = MatSetValue(A,i, i + cp1idu + jrd , -cp1id_usign * muinva/hh, ADD_VALUES); CHKERRQ(ierr);
-      ierr = MatSetValue(A,i, i + jrd , +(muinva + muinvl)/hh, ADD_VALUES); CHKERRQ(ierr);
-      ierr = MatSetValue(A,i, i - cp1idl + jrd , - cp1id_lsign * muinvl/hh, ADD_VALUES); CHKERRQ(ierr);
+      ierr = MatSetValue(A,i, i + cp1idu + jrd , c1, ADD_VALUES); CHKERRQ(ierr);
+      ierr = MatSetValue(A,i, i + jrd , c2, ADD_VALUES); CHKERRQ(ierr);
+      ierr = MatSetValue(A,i, i - cp1idl + jrd , c3, ADD_VALUES); CHKERRQ(ierr);
 
 
       /* d/dz muinv d/dx Ez */
@@ -189,46 +199,60 @@ PetscErrorCode MoperatorGeneral(MPI_Comm comm, Mat *Aout, int Nx, int Ny, int Nz
       if(ixyz[ic] == Nxyz[ic]-1)
 	{
 	  if(ic == (DimPeriod-1) || DimPeriod == 4 || (DimPeriod<0 && ic!=-(DimPeriod+1)) )
-	    cidu = (1-Nxyz[ic])*cid;
+	    {
+	      cidu = (1-Nxyz[ic])*cid;
+	      cidu_phase = blochbc[ic][0] + I * blochbc[ic][1];
+	    }
 	  else
 	    {
 	      cidu = 0;
-	      cid_usign = b[ic][1][cp2];
+	      cidu_phase = b[ic][1][cp2];
 	    }
 	}
 
-      muinva = muinv[icp1 + jrdmu];
+      muinvcp1cmp = muinv[icp1%Nxyzc] + I*muinv[icp1%Nxyzc + Nxyzc];
 
       if(ixyz[cp2] == 0)
 	{
 
 	  if(cp2 == (DimPeriod-1) || DimPeriod == 4 || (DimPeriod<0 && cp2!=-(DimPeriod+1)) )
-	    cp2idl = (1-Nxyz[cp2])*cp2id;
+	    {
+	      cp2idl = (1-Nxyz[cp2])*cp2id;
+	      cp2idl_phase = blochbc[cp2][0] - I*blochbc[cp2][1];
+	    }
 	  else
 	    {
 	      cp2idl = 0; 
-	      cp2id_lsign = b[cp2][0][cp2];		   
+	      cp2idl_phase = b[cp2][0][cp2];		   
 	    }
 	}
 
-       muinvl = muinv[icp1 - cp2idl + jrdmu];
+      muinvcp1lcmp = muinv[(icp1-cp2idl)%Nxyzc] + I*muinv[(icp1-cp2idl)%Nxyzc + Nxyzc];
+ 
+      hh = h[ic]*h[cp2];
+      c1 = creal(cidu_phase * muinvcp1cmp * magicnum)/hh;
+      c2 = -creal(muinvcp1cmp * magicnum)/hh;
+      c3 = -creal(cidu_phase * cp2idl_phase * muinvcp1lcmp * magicnum)/hh;
+      c4 = creal(cp2idl_phase * muinvcp1lcmp * magicnum)/hh;
 
-      hh = sign * h[ic]*h[cp2];
-      ierr = MatSetValue(A,i, icp2 + cidu + jrd, cid_usign * muinva/hh, ADD_VALUES); CHKERRQ(ierr);          ierr = MatSetValue(A,i, icp2 + jrd, -muinva/hh, ADD_VALUES); CHKERRQ(ierr); 
-      ierr = MatSetValue(A,i, icp2 + cidu - cp2idl + jrd, - cid_usign * cp2id_lsign * muinvl/hh, ADD_VALUES); CHKERRQ(ierr);
-      ierr = MatSetValue(A,i, icp2 - cp2idl + jrd, + cp2id_lsign*muinvl/hh, ADD_VALUES); CHKERRQ(ierr);
+      ierr = MatSetValue(A,i, icp2 + cidu + jrd, c1, ADD_VALUES); CHKERRQ(ierr);          ierr = MatSetValue(A,i, icp2 + jrd, c2, ADD_VALUES); CHKERRQ(ierr); 
+      ierr = MatSetValue(A,i, icp2 + cidu - cp2idl + jrd, c3, ADD_VALUES); CHKERRQ(ierr);
+      ierr = MatSetValue(A,i, icp2 - cp2idl + jrd, c4, ADD_VALUES); CHKERRQ(ierr);
 	       
       /* - d/dz muinv d/dz Ex */
-	 
-      muinva = muinv[icp1 + jrdmu];
+      // it shares same muinvcp1cmp and muinvcp1lcmp as d/dz muinv d/dx Ez;
+     
       if(ixyz[cp2] == Nxyz[cp2]-1)
 	{
 	  if (cp2== (DimPeriod-1) || DimPeriod == 4 || (DimPeriod<0 && cp2 !=-(DimPeriod+1)) )
-	    cp2idu = (1-Nxyz[cp2])*cp2id;
+	    {
+	      cp2idu = (1-Nxyz[cp2])*cp2id;
+	      cp2idu_phase = blochbc[cp2][0] + I * blochbc[cp2][1];
+	    }
 	  else
 	    {
 	      cp2idu = 0;
-	      cp2id_usign = b[cp2][1][ic];
+	      cp2idu_phase = b[cp2][1][ic];
 	    }
 	}
 	       
@@ -238,25 +262,27 @@ PetscErrorCode MoperatorGeneral(MPI_Comm comm, Mat *Aout, int Nx, int Ny, int Nz
 	  if ( cp2!= (DimPeriod-1) && DimPeriod !=4 && ( DimPeriod >=0 || cp2 == -(DimPeriod+1)) )
 	    {
 	      cp2idl = -cp2idu;  
-	      cp2id_lsign = b[cp2][0][ic];
-	      muinvl = muinva;
+	      cp2idl_phase = b[cp2][0][ic];
 	    }
 	  else
 	    {
 	      cp2idl = (1-Nxyz[cp2])*cp2id;
-	      muinvl = muinv[icp1 - cp2idl + jrdmu]; 
+	      cp2idl_phase = blochbc[cp2][0] - I*blochbc[cp2][1];
 	    }
 	}
-      else
-	{
-	  muinvl = muinv[icp1 - cp2idl + jrdmu];
-	}
+   
 
-      hh = sign * h[cp2]*h[cp2];
+      hh = h[cp2]*h[cp2];
+      
+      c1 = -creal(muinvcp1cmp * cp2idu_phase * magicnum)/hh;
+      c2 = +creal((muinvcp1cmp + muinvcp1lcmp)*magicnum)/hh;
+      c3 = -creal(muinvcp1lcmp * cp2idl_phase * magicnum)/hh;
+   
+      ierr = MatSetValue(A,i, i + cp2idu + jrd, c1, ADD_VALUES); CHKERRQ(ierr);
+      ierr = MatSetValue(A,i, i + jrd, c2, ADD_VALUES); CHKERRQ(ierr);
+      ierr = MatSetValue(A,i, i - cp2idl + jrd, c3, ADD_VALUES); CHKERRQ(ierr);
 
-      ierr = MatSetValue(A,i, i + cp2idu + jrd, -cp2id_usign*muinva/hh, ADD_VALUES); CHKERRQ(ierr);
-      ierr = MatSetValue(A,i, i + jrd, +(muinva + muinvl)/hh, ADD_VALUES); CHKERRQ(ierr);
-      ierr = MatSetValue(A,i, i - cp2idl + jrd, -cp2id_lsign* muinvl/hh, ADD_VALUES); CHKERRQ(ierr);
+
 
       /*---add tiny number to diagonals to keep nonzero positions on diagonal for future---*/
       if (flg && (jrd!=0))      
