5337d11467cabdc40ddb46f5a10d33b29538fa44
xdliang <xdliang@mit.edu>
2012-03-11 17:33:06 -0400

    implemented SolarEigenvaluesSolver.c to compute eigenvalues
    Ignore-this: 586547604eb43a1885f8ea964cbfc0ad
    
    darcs-hash:20120311213306-88e91-65488bd453f8ba4a25cd105b6246752aefe77180.gz

---
+++ CodesForLibrary/SolarEigenvaluesSolver.c
@@ -0,0 +1,87 @@
+#include <petsc.h>
+#include "slepceps.h"
+#include "Resonator.h"
+
+
+/*---this subrountie compute the eigenvalues lambda of the generalized eigenvalue problem (M-eps*omega_0^2) V = eps*lambda * V. In order to compute with mpb, user need to convert like this sqrt(lambda+omega_0^2)/(2*pi). */
+
+#undef _FUNCT_
+#define _FUNCT_ "SolarEigenvalueSolver"
+int SolarEigenvaluesSolver(Mat M, Vec epsCurrent, Vec epspmlQ, Mat D)
+{
+
+  PetscErrorCode ierr;
+  EPS eps;
+  const EPSType type;
+  PetscReal error, tol, re, im;
+  PetscScalar kr,  ki;
+  PetscInt nconv;
+
+  Mat B;
+  int nrow, ncol;
+
+  ierr=MatGetSize(M,&nrow, &ncol); CHKERRQ(ierr);
+
+  ierr=MatCreateMPIAIJ(PETSC_COMM_WORLD, PETSC_DECIDE, PETSC_DECIDE, nrow, ncol, 1, NULL, 1, NULL, &B); CHKERRQ(ierr);
+  ierr=PetscObjectSetName((PetscObject)B, "epsmatrix"); CHKERRQ(ierr);
+
+  if (D==PETSC_NULL)
+    {  // for purely real epsC, no absorption;
+      ierr=MatDiagonalSet(B,epsCurrent,INSERT_VALUES); CHKERRQ(ierr);
+      MatAssemblyBegin(B,MAT_FINAL_ASSEMBLY);
+      MatAssemblyEnd(B,MAT_FINAL_ASSEMBLY);
+    }
+  else
+    {
+      Vec epsC;
+      VecDuplicate(epsCurrent, &epsC);
+      ierr = VecPointwiseMult(epsC, epsCurrent,epspmlQ); CHKERRQ(ierr);
+      
+      MatSetTwoDiagonals(B, epsC, D, 1.0);
+      VecDestroy(epsC);
+    }
+
+  PetscPrintf(PETSC_COMM_WORLD,"!!!---computing eigenvalues---!!! \n");
+  ierr=EPSCreate(PETSC_COMM_WORLD, &eps); CHKERRQ(ierr);
+  ierr=EPSSetOperators(eps, M, B); CHKERRQ(ierr);
+  //ierr=EPSSetProblemType(eps,EPS_PGNHEP);CHKERRQ(ierr);
+  EPSSetFromOptions(eps);
+  ierr=EPSSolve(eps); CHKERRQ(ierr);
+  EPSGetConverged(eps, &nconv); CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_WORLD,"Number of converged eigenpairs: %d\n\n",nconv);CHKERRQ(ierr);
+
+
+  double *krarray, *kiarray, *errorarray;
+  krarray = (double *) malloc(sizeof(double)*nconv);
+  kiarray = (double *) malloc(sizeof(double)*nconv);
+  errorarray =(double *) malloc(sizeof(double)*nconv);
+
+  int ni;
+  for(ni=0; ni<nconv; ni++)
+    {
+      ierr=EPSGetEigenpair(eps,ni, krarray+ni,kiarray+ni,PETSC_NULL,PETSC_NULL);CHKERRQ(ierr);
+      ierr = EPSComputeRelativeError(eps,ni,errorarray+ni);CHKERRQ(ierr);
+      ierr=EPSComputeRelativeError(eps, ni, errorarray+ni );
+    }
+
+  PetscPrintf(PETSC_COMM_WORLD, "Now print the eigenvalues: \n");
+  for(ni=0; ni<nconv; ni++)
+    PetscPrintf(PETSC_COMM_WORLD," %g%+gi,", krarray[ni], kiarray[ni]);
+
+  PetscPrintf(PETSC_COMM_WORLD, "\n\nstart printing erros");
+
+  for(ni=0; ni<nconv; ni++)
+    PetscPrintf(PETSC_COMM_WORLD," %g,", errorarray[ni]);      
+
+  PetscPrintf(PETSC_COMM_WORLD,"\n\n Finish EPS Solving !!! \n\n");
+
+  /*-- destroy vectors and free space --*/
+  EPSDestroy(eps);
+  MatDestroy(B);
+
+  free(krarray);
+  free(kiarray);
+  free(errorarray);
+
+  PetscFunctionReturn(0);
+}
